705d9fa77c59dca95a7db3c213ae4e44adc3662f
kurbanovxurshidbek <shamsun.com@gmail.com>
2025-01-24 19:35:22 +0500

    Firebase settings implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:instaclone/config/root_binding.dart';
 import 'package:instaclone/pages/home_page.dart';
 import 'package:instaclone/pages/splash_page.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
